5ffebb1c7e9d7d5e5fa985a4d1e336b4968d6642
Klara Bühr <klara.buehr@hfg-gmuend.de>
2026-04-13 23:42:31 +0200

    pixel video - particles show the clear video

--- Computer_Vision/video skeleton/sketch.js
+++ Computer_Vision/video skeleton/sketch.js
@@ -35,7 +35,7 @@ function gotPoses(results) {
 
 function draw() {
   // Draw the webcam video
-  image(video, 0, 0, width, height);
+  drawPixelatedVideo();
 
   // Draw the skeleton connections
   for (let i = 0; i < poses.length; i++) {
@@ -47,7 +47,7 @@ function draw() {
       let pointB = pose.keypoints[pointBIndex];
       // Only draw a line if both points are confident enough
       if (pointA.confidence > 0.1 && pointB.confidence > 0.1) {
-        stroke(250, 0, 0);
+        stroke(250, 0, 0, 0);
         strokeWeight(2);
         line(pointA.x, pointA.y, pointB.x, pointB.y);
       }
@@ -61,7 +61,7 @@ function draw() {
       let keypoint = pose.keypoints[j];
       // Only draw a circle if the keypoint's confidence is bigger than 0.1
       if (keypoint.confidence > 0.1) {
-        fill(0, 250, 0);
+        fill(0, 250, 0, 0);
         noStroke();
         circle(keypoint.x, keypoint.y, 10);
       }
@@ -89,7 +89,7 @@ for (let i = particles.length - 1; i >= 0; i--) {
     
     //draw line
     strokeWeight(4);
-    stroke(255);
+    stroke(255, 0, 0, 0);
     line(leftHand.x,leftHand.y,rightHand.x,rightHand.y);
 
 
@@ -108,12 +108,12 @@ let centerY = (leftHand.y + rightHand.y) / 2;
 
 // Abstand auf Anzahl der Partikel übertragen
 let spawnCount = int(map(handDistance, 20, 500, 1, 20, true));
-let particleSize = map(handDistance, 20, 500, 1, 70, true);
+let particleSize = map(handDistance, 20, 500, 1, 150, true);
 
-let r = map(handDistance, 20, 500, 0, 255, true);
+/* let r = map(handDistance, 20, 500, 0, 255, true);
 let g = map(handDistance, 20, 500, 100, 120, true);
 let b = map(handDistance, 20, 500, 255, 150, true);
-
+ */
 // neue Partikel erzeugen
 if (frameCount % 15 === 0) {
 for (let i = 0; i < spawnCount; i++) {
@@ -129,7 +129,7 @@ for (let i = 0; i < spawnCount; i++) {
     let leftEllbow = createVector(poses[0].keypoints[7].x, poses[0].keypoints[7].y);
     let leftShoulder = createVector(poses[0].keypoints[5].x, poses[0].keypoints[5].y);
 
-    stroke(0,0,255);
+    stroke(0,0,255, 0);
     line(leftHand.x,leftHand.y,leftEllbow.x,leftEllbow.y);
     line(leftShoulder.x,leftShoulder.y,leftEllbow.x,leftEllbow.y);
   }
@@ -137,20 +137,14 @@ for (let i = 0; i < spawnCount; i++) {
 
 
 class Particle {
-  constructor(x, y, size, r, g, b) {
+  constructor(x, y, size) {
     this.x = x;
     this.y = y;
     this.vx = random(-2, 2);
     this.vy = random(-2, 2);
     this.size = size;
     this.transparency = 255;
-
-      //Farbe der Partikel
-      this.r = r;
-      this.g = g;
-      this.b = b;
   }
-
 
   update() {
     this.x += this.vx;
@@ -159,12 +153,42 @@ class Particle {
   }
 
   display() {
-    noStroke();
-    fill(this.r, this.g, this.b, this.transparency);
-    ellipse(this.x, this.y, this.size);
+    push();
+    tint(255, this.transparency);
+
+    copy(
+      video,
+      this.x - this.size / 2, this.y - this.size / 2,   // Quelle im Video
+      this.size, this.size,                              // Größe des Ausschnitts
+      this.x - this.size / 2, this.y - this.size / 2,   // Ziel auf Canvas
+      this.size, this.size
+    );
+
+    pop();
   }
 
   isFinished() {
     return this.transparency <= 0;
+  }
+}
+
+function drawPixelatedVideo() {
+  background(0);
+  video.loadPixels();
+
+  let stepSize = 15;
+
+  for (let y = 0; y < height; y += stepSize) {
+    for (let x = 0; x < width; x += stepSize) {
+      let index = (x + y * width) * 4;
+
+      let r = video.pixels[index];
+      let g = video.pixels[index + 1];
+      let b = video.pixels[index + 2];
+
+      fill(r, g, b);
+      noStroke();
+      rect(x, y, stepSize, stepSize);
+    }
   }
 }
